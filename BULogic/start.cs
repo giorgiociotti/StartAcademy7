2dcf9bb7f3d3c00a4e467ddafc8b8fc7458d01c2
giorgio <gciotti@gmail.com>
2025-03-10 15:47:17 +0100

    add switch

--- BULogic/start.cs
+++ BULogic/start.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StartAcademy7.BLogic;
+using System;
 using System.Text;
 
 namespace StartAcademy7.BLogic
@@ -21,6 +22,7 @@ namespace StartAcademy7.BLogic
                 {
                     Console.Clear();
                     WriteMenu(menuDescription, msg);
+                    menu();  // Aggiunto per chiamare il menu dopo aver visualizzato la descrizione
                 }
                 else
                 {
@@ -50,6 +52,64 @@ namespace StartAcademy7.BLogic
             Console.WriteLine(new string('-', menuDescription.Length));
             Console.WriteLine("\nPremi INVIO per continuare...");
             Console.ReadLine();
+        }
+
+        static void menu()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Scegli un'opzione:");
+                Console.WriteLine("A - test iterazioni");
+                Console.WriteLine("X - uscita");
+                Console.Write("Scelta: ");
+
+                string input = Console.ReadLine().ToUpper();
+                Console.Clear();
+
+
+                switch (input)
+                {
+                    case "A":
+                        Console.Clear();
+                        DemoIterations();
+                        break;
+
+                    case "X":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Scelta non valida, inserisci A per i test delle iterazioni o X per uscire.");
+                        break;
+                }
+            }
+        }
+
+        static void DemoIterations()
+        {
+            Console.WriteLine("Esecuzione Metodi classe Iterations");
+
+            string[] strings = ["cavallo", "cane", "gatto"];
+
+            Iterations iterations = new(10, strings);
+
+            Console.WriteLine("Esecuzione ForEachIteration");
+            iterations.ForEachIteration();
+            Console.WriteLine("-------------------------------------------------------");
+            
+            Console.WriteLine("Esecuzione ForIteration");
+            iterations.ForIteration();
+            Console.WriteLine("-------------------------------------------------------");
+
+            Console.WriteLine("Esecuzione WhileDoIteration");
+            iterations.WhileDoIteration();
+            Console.WriteLine("-------------------------------------------------------");
+
+            Console.WriteLine("Esecuzione DoWhileIteration");
+            iterations.DoWhileIteration();
         }
         #endregion
     }
--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 
 namespace StartAcademy7
 {
-    //internal significa che viene condiviso solo all'interno dello stesso progetto
-    //start application class
+    // start application class
     internal class Program
     {
+
         /// <summary>
         /// Main entry point
         /// </summary>
