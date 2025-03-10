ca2f0500ffee039a3bafb9e80c727c3812352d0b
giorgio <gciotti@gmail.com>
2025-03-10 13:21:21 +0100

    Aggiungere i file di progetto.

---
+++ BULogic/start.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartAcademy7.BLogic
+{
+    internal static class Start
+    {
+        #region Private Variables
+        #endregion
+
+        #region Public Methods
+        internal static void ShowMenu()
+        {
+            string? menuDescription = "Benvenuto nel menu principale!";
+            Console.WriteLine("Inizio academy!");
+            Console.Write("Inserire nominativo: ");
+            string? msg = Console.ReadLine();
+
+            if (msg != null && msg.Length>0)
+            {
+                WriteMenu(menuDescription, msg);
+            }
+            else
+            {
+                Console.WriteLine("Nominativo non inserito");
+            }
+
+
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// It write the main menu on console
+        /// </summary>
+        /// <param name="menuDescription"></param>
+        /// <param name="msg"></param>
+        static void WriteMenu(string menuDescription, string msg)//Senza specificare sarà private
+        {
+            Console.WriteLine($"Benvenuto, caro: {msg.ToUpper()}");
+            Console.Clear();
+            Console.WriteLine(new string('-', menuDescription.Length - 1));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(menuDescription);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(new string('-', menuDescription.Length - 1));
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using StartAcademy7.BLogic;
+
+namespace StartAcademy7
+{
+    //internal significa che viene condiviso solo all'interno dello stesso progetto
+    //start application class
+    internal class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Start.ShowMenu();
+        }
+    }
+}
